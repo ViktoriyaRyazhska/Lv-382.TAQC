6588f5a68b45e03c1e9e9fb74ede38e9aad234bf
Valentin Lobanov <vdlobanovs@gmail.com>
2019-02-23 14:27:35 +0200

    Some chenges in inpoint

--- SoftTasks/SoftTasks/Methods.cs
+++ SoftTasks/SoftTasks/Methods.cs
@@ -38,5 +38,7 @@ namespace SoftTasks
             }
             return fsum;
         }
+
+
     }
 }
--- SoftTasks/SoftTasks/Program.cs
+++ SoftTasks/SoftTasks/Program.cs
@@ -8,9 +8,11 @@ namespace SoftTasks
 {
     class Program
     {
+        public delegate int InvokeMeth(int i);
 
         static void Main(string[] args)
         {
+            InvokeMeth a;
             while (true)
             {
                 Console.WriteLine("Enter task number");
@@ -20,23 +22,28 @@ namespace SoftTasks
                 {
                     break;
                 }
-                switch (index)  // Depends on number invoke method. Add your method here.
+                switch (index)  // Invoke method depents on number. Add your methods here.
                 {
                     case "1":
-                        Console.WriteLine("Enter number");    
-                        Console.WriteLine(Methods.Fibbonachi(int.Parse(Console.ReadLine().ToString())));
-                        Console.ReadKey();
+                        a = Methods.Fibbonachi;          // Delegate that contain your Method. Add according to template or write custome. 
+                        Print(a);
                         break;
                     case "2":
-                        Console.WriteLine("Enter number");
-                        Console.WriteLine(Methods.Mod_Fibbonachi(int.Parse(Console.ReadLine().ToString())));
-                        Console.ReadKey();
+                        a = Methods.Mod_Fibbonachi;
+                        Print(a);
                         break;
                 }
                 Console.Clear();
 
             }
 
+        }
+        public static void Print(InvokeMeth a)
+        {
+            Console.WriteLine("Enter number");
+            int n = int.Parse(Console.ReadLine().ToString());
+            Console.WriteLine(a.Invoke(n));
+            Console.ReadKey();
         }
     }
 }
