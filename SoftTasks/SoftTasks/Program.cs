f7d530fbf2aae1258598f9806e1ac1bc9b9ad0d0
SerhiiZakharchuk <serhiizakharchuk@gmail.com>
2019-02-23 11:24:38 +0200

    some titles in start page

--- SoftTasks/SoftTasks/Program.cs
+++ SoftTasks/SoftTasks/Program.cs
@@ -13,17 +13,18 @@ namespace SoftTasks
 
             while (true)
             {
+                Console.WriteLine("Enter task number");
                 string index = Console.ReadLine();
                 switch (index)
                 {
                     case "0":
                         break;
                     case "1":
-                        Console.WriteLine("Enter fib number");
+                        Console.WriteLine("Enter fib 1 number");
                         Console.WriteLine(Methods.Fibonachi(int.Parse(Console.ReadLine().ToString())));
                         break;
                     case "2":
-                        Console.WriteLine("Enter fib number");
+                        Console.WriteLine("Enter fib 2 number");
                         Console.WriteLine(Methods.Fibonachi2(int.Parse(Console.ReadLine().ToString())));
                         break;
                 }
