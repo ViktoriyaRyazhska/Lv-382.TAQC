c59160ee8fa3eb967d7b3fb0522cb2c30f4d3c73
khristinaf <khristina.fedun@gmail.com>
2019-02-24 13:17:50 +0200

    Positive Sum

---
+++ SoftTasks/SoftTasks/CodeWars/Level8/PositiveSum.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftTasks.CodeWars.Level8
+{
+    class PositiveSum
+    {
+        public static int GetPositiveSum(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0)
+                    sum += arr[i];
+            }
+
+            return sum;
+        }
+
+    }
+
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        [TestCase(new int[] { 1, 2, 3, 4, 5 }, ExpectedResult = 15)]
+        [TestCase(new int[] { 1, -2, 3, 4, 5 }, ExpectedResult = 13)]
+        [TestCase(new int[] { -1, 2, 3, 4, -5 }, ExpectedResult = 9)]
+        [TestCase(new int[] { }, ExpectedResult = 0)]
+        [TestCase(new int[] { -1, -2, -3, -4, -5 }, ExpectedResult = 0)]
+        public static int ExampleTest(int[] arr)
+        {
+            return PositiveSum.GetPositiveSum(arr);
+        }
+    }
+}
--- SoftTasks/SoftTasks/Program.cs
+++ SoftTasks/SoftTasks/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,72 +9,11 @@ namespace SoftTasks
 {
     class Program
     {
-        public delegate int InvokeMeth(int i);
-
         static void Main(string[] args)
         {
-            InvokeMeth a;
-            while (true)
-            {
-                Console.WriteLine("Enter task number");
-                foreach (string str in Methods.MethodsList)
-                    Console.WriteLine(str);                 // List of methods
-                string index = Console.ReadLine();
-                if (index == "0")         // Exit
-                {
-                    break;
-                }
+          
 
-                switch (index)  // Invoke method depents on number. Add your methods here as a case.
-                {
-                    case "1":
-                        a = Methods.Fibbonachi;          // Delegate that contain your Method. Add according to template or write custome. 
-                        Print(a);
-                        break;
-                    case "2":
-                        a = Methods.Mod_Fibbonachi;
-                        Print(a);
-                        break;
-                    case "3":
-                        a = Methods.WayToCoverIn3Steps;
-                        Print(a);
-                        break;
-                    case "4":
-                        a = Methods.FriendPairs;
-                        Print(a);
-                        break;
-                    case "5":
-                        a = Methods.CountWays;
-                        Print(a);
-                        break;
-                    case "8":
-                        a = Methods.countAllWays;
-                        Print(a);
-                        break;
-                }
-                Console.Clear();
 
-            }
-
-        }
-
-        public static void Print(InvokeMeth a)
-        {
-            int n;
-            Console.WriteLine("Enter number");
-            try
-            {
-                n = int.Parse(Console.ReadLine().ToString());             
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Invalid input");
-                Console.ReadKey();
-                return;
-            }
-
-            Console.WriteLine("Output:" + "\n" + a.Invoke(n));
-            Console.ReadKey();
         }
     }
 }
