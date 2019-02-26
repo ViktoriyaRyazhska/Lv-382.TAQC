8154ce498d10d8e014d60907b2d50f9cc35c9943
SerhiiZakharchuk <serhiizakharchuk@gmail.com>
2019-02-26 17:09:42 +0200

    ways3steps_validtest

--- SoftTasks/SoftTasks/Tests/SoftTaskTests.cs
+++ SoftTasks/SoftTasks/Tests/SoftTaskTests.cs
@@ -15,5 +15,19 @@ namespace SoftTasks.Tests
         {
             /////lalalalal
         }
+        [Test]
+        public void WayToCoverIn3Steps_ValidInput_Test1()
+        {
+            // Arrange
+            int input1 = 3;
+            int expected = 4;
+
+            // Act
+            int actual = Methods.WayToCoverIn3Steps(input1);
+
+            // Assert
+           
+            Assert.AreEqual(expected, actual, $"WayToCoverIn3Steps failed with valid data{input1}");
+        }
     }
 }
