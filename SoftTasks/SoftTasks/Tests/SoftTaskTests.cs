81f3a30605e54b9c2ee073db7b67e359d14c6bf1
Natalia Voloshyna <nataliya.voloshyna1908@gmail.com>
2019-02-26 18:45:46 +0200

    add tests for InterestingRows

--- SoftTasks/SoftTasks/Tests/SoftTaskTests.cs
+++ SoftTasks/SoftTasks/Tests/SoftTaskTests.cs
@@ -10,10 +10,53 @@ namespace SoftTasks.Tests
     [TestFixture]
     public class SoftTaskTests
     {
-        [Test]
-        public void MyTestMethod()
+        [Test]      
+        public void InterestingRows_1_1returned()
         {
-            /////lalalalal
+            int inputNumb = 1;
+            int expected = 1;
+            int actual = Methods.Interesting_Rows(inputNumb);
+            Assert.AreEqual(expected, actual, 0.001, "Expected result and actual result are different");
+        }
+        [Test]
+        public void InterestingRows_2_1returned()
+        {
+            int inputNumb = 2;
+            int expected = 1;
+            int actual = Methods.Interesting_Rows(inputNumb);
+            Assert.AreEqual(expected, actual, 0.001, "Expected result and actual result are different");
+        }
+        [Test]
+        public void InterestingRows_5_11returned()
+        {
+            int inputNumb = 5;
+            int expected = 11;
+            int actual = Methods.Interesting_Rows(inputNumb);
+            Assert.AreEqual(expected, actual, 0.001, "Expected result and actual result are different");
+        }
+        [Test]
+        public void InterestingRows_40_1431655765returned()
+        {
+            int inputNumb = 40;
+            int expected = 1431655765;
+            int actual = Methods.Interesting_Rows(inputNumb);
+            Assert.AreEqual(expected, actual, 0.001, "Expected result and actual result are different");
+        }
+        [Test]
+        public void InterestingRows_0_0returned()
+        {
+            int inputNumb = 0;
+            int expected = 0;
+            int actual = Methods.Interesting_Rows(inputNumb);
+            Assert.AreEqual(expected, actual, 0.001, "Expected result and actual result are different");
+        }
+        [Test]
+        public void InterestingRows_0returned()
+        {
+            int inputNumb = -9;
+            int expected = 0;
+            int actual = Methods.Interesting_Rows(inputNumb);
+            Assert.AreEqual(expected, actual, 0.001, "Expected result and actual result are different");
         }
     }
 }
