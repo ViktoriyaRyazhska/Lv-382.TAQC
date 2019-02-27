28b769e6290425af30941a51e196b47fa6fea720
SerhiiZakharchuk <serhiizakharchuk@gmail.com>
2019-02-28 00:07:55 +0200

    add array2 task

---
+++ Tasks for Core/Extra tasks/Extra tasks/Array2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extra_tasks
+{
+    class Array2
+    {
+        public static void Row(int n)
+        {
+            if (n > 0)
+            {
+                double[] arr = new double[n];
+                double ar0 = 0d;
+                double ar1 = (double)5 / 8;
+
+                if (n > 2)
+                {
+                    arr[0] = ar0;
+                    arr[1] = ar1;
+                    for (int i = 2; i < n; i++)
+                    {
+                        arr[i] = arr[i - 1] / 2d + arr[i - 2] * 3d / 4d;
+                    }
+                }
+                else if (n == 2)
+                {
+                    arr[0] = 0;
+                    arr[1] = ar1;
+                }
+                else if (n == 1)
+                {
+                    arr[0] = 0;
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("arr[" + i + "]= " + arr[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter elements quantity >= 1");
+            }
+        }
+    }
+}
--- Tasks for Core/Extra tasks/Extra tasks/Program.cs
+++ Tasks for Core/Extra tasks/Extra tasks/Program.cs
@@ -10,8 +10,12 @@ namespace Extra_tasks
     {
         static void Main(string[] args)
         {
-            Array1.Century();
+            //Array1.Century();
+            //Console.ReadKey();
+
+            Array2.Row(20);
             Console.ReadKey();
+
             //Weather weater = new Weather(new WeatherForDay(-2, 751, "p", "2"), new WeatherForDay(3, 743, "l", "2")); //1
             //weater.PrintAll();
 
