a3b323b17109f8e8f26efe1346982b0ac83f18d8
SerhiiZakharchuk <serhiizakharchuk@gmail.com>
2019-02-27 22:57:15 +0200

    Array task1

---
+++ Tasks for Core/Extra tasks/Extra tasks/Array1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extra_tasks
+{
+    class Array1
+    {
+        public static int Century()
+        {
+            Console.Write("Enter year: ");
+            int z;
+            int x = int.Parse(Console.ReadLine());
+            if (x > 0)
+            {
+                double y = (x / 100.00d);
+                if ((y % 1) > 0.00d)
+                {
+                    z = (int)(y + 1);
+                    Console.WriteLine(z);
+                    return z;
+                }
+                else
+                {
+                    Console.WriteLine(y);
+                    return (int)y;
+                }
+            }
+            
+            Console.WriteLine("Enter year > 0");
+            return 0;
+            
+        }
+    }
+}
--- Tasks for Core/Extra tasks/Extra tasks/Program.cs
+++ Tasks for Core/Extra tasks/Extra tasks/Program.cs
@@ -10,10 +10,12 @@ namespace Extra_tasks
     {
         static void Main(string[] args)
         {
-            Weather weater = new Weather(new WeatherForDay(-2, 751, "p", "2"), new WeatherForDay(3, 743, "l", "2"));
-            weater.PrintAll();
-
+            Array1.Century();
             Console.ReadKey();
+            //Weather weater = new Weather(new WeatherForDay(-2, 751, "p", "2"), new WeatherForDay(3, 743, "l", "2")); //1
+            //weater.PrintAll();
+
+            //Console.ReadKey();
         }
     }
 }
