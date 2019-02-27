5de4849d34d86ab838cbcfb41bd952fc5288beb3
Roman Pihel <romanpihel1992@gmail.com>
2019-02-27 19:28:50 +0200

    2Tasks

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            public static char GetChar(int charcode)
+            {
+                return (char)charcode;
+            }
+            //1
+
+            public static int TotalPoints(string[] games)
+            {
+                int points = 0;
+
+                foreach (string a in games)
+                {
+                    int x = System.Convert.ToInt32(a.Split(':')[0]);
+                    int y = System.Convert.ToInt32(a.Split(':')[1]);
+                    if (x > y) points += 3;
+                    else if (x < y) points += 0;
+                    else points += 1;
+                }
+                return points;
+            }
+            //2
+        }   
+    }
+}
