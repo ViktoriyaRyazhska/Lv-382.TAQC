352bc05b8d05b5d98ad36949117244073a648de5
SerhiiZakharchuk <serhiizakharchuk@gmail.com>
2019-02-28 11:27:05 +0200

    array8 task

---
+++ Tasks for Core/Extra tasks/Extra tasks/Array7.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extra_tasks
+{
+    class Array7
+    {
+        public static void NumberK(int k)
+        {
+            string row="";
+            int[] arr = new int[90];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i+10;
+                row += arr[i];
+            }
+
+            Console.WriteLine(row.Substring(k-1,1));
+        }
+    }
+}
--- Tasks for Core/Extra tasks/Extra tasks/Program.cs
+++ Tasks for Core/Extra tasks/Extra tasks/Program.cs
@@ -25,7 +25,10 @@ namespace Extra_tasks
             //Array5.TriangleType();
             //Console.ReadKey();
 
-            Array6.NotEqualNumber();
+            //Array6.NotEqualNumber();
+            //Console.ReadKey();
+
+            Array7.NumberK(8);
             Console.ReadKey();
 
             //Weather weater = new Weather(new WeatherForDay(-2, 751, "p", "2"), new WeatherForDay(3, 743, "l", "2")); //1
