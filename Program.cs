5aa913962155cacf2337878c646d6b41ef3ac9cc
Roman Pihel <romanpihel1992@gmail.com>
2019-02-28 02:32:59 +0200

    tasks to 31

--- Program.cs
+++ Program.cs
@@ -176,6 +176,220 @@ namespace CodeWars
                 else return -1 * number;
             }
             //16
+            public static string boolToWord(bool word)
+            {
+                if (word) return "Yes";
+                else return "No";
+            }
+            //17
+
+            public static double basicOp(char operation, double value1, double value2)
+            {
+                if (operation == '+') return value1 + value2;
+                else if (operation == '-') return value1 - value2;
+                else if (operation == '*') return value1 * value2;
+                else return value1 / value2;
+            }
+            //18
+
+            public int GetSum(int a, int b)
+            {
+                int sum = 0;
+                if (a < b)
+                {
+                    for (int i = a; i <= b; i++)
+                        sum += i;
+                }
+                else if (a > b)
+                {
+                    for (int i = b; i <= a; i++)
+                        sum += i;
+                }
+                else sum = a;
+                return sum;
+            }
+            //19
+
+            public static string seriesSum(int n)
+            {
+                decimal z = 1;
+                decimal sum = 0;
+                if (n == 0)
+                {
+                    sum = 0.000001m;
+                }
+                for (int i = 0; i < n; i++)
+                {
+                    sum += (1 / z);
+                    z += 3;
+                }
+                return System.Convert.ToString(Math.Round(sum, 2));
+            }
+            //20
+
+            public static bool IsSquare(int n)
+            {
+                int k = (int)Math.Sqrt(n);
+                if (k * k == n) return true;
+                else return false;
+            }
+            //21
+
+            public static string HighAndLow(string numbers)
+            {
+                string[] tokens = numbers.Split(' ');
+                int[] ints = Array.ConvertAll<string, int>(tokens, int.Parse);
+                int t1 = ints[0];
+                int t2 = ints[0];
+                for (int i = 0; i < ints.Length; i++)
+                {
+                    if (t1 < ints[i])
+                    {
+                        t1 = ints[i];
+                    }
+                    if (t2 > ints[i])
+                    {
+                        t2 = ints[i];
+                    }
+                }
+                int[] rez = new int[2];
+                rez[0] = t1;
+                rez[1] = t2;
+                string result = string.Join(" ", rez);
+                return result;
+            }
+            //22
+
+            public static int NbYear(int p0, double percent, int aug, int p)
+            {
+                int year = 0;
+                while (p0 < p)
+                {
+                    p0 += Math.Truncate(p0 * (percent / 100)) + aug;
+                    year++;
+                }
+                return year;
+            }
+            //23
+
+            public static int GetVowelCount(string str)
+            {
+                int vowelCount = 0;
+                for (int i = 0; i <= str.Length - 1; i++)
+                {
+                    if (str.Substring(i, 1).Equals("a") || str.Substring(i, 1).Equals("e")
+                    || str.Substring(i, 1).Equals("i") || str.Substring(i, 1).Equals("o")
+                    || str.Substring(i, 1).Equals("u")) vowelCount++;
+                }
+                return vowelCount;
+            }
+            //24
+
+            public static int FindShort(string s)
+            {
+                string[] ss = s.Split(new char[] { ' ' });
+                int c1 = s.Length;
+                for (int i = 0; i < ss.Length; i++)
+                {
+                    if (ss[i].Length < c1) { c1 = ss[i].Length; }
+                }
+                return c1;
+            }
+            //25
+
+            public static String Accum(string s)
+            {
+                char[] a = s.ToUpper().ToCharArray();
+                string rez = a[0].ToString();
+                for (int i = 1; i < a.Length; i++)
+                {
+                    rez += "-" + a[i];
+                    for (int j = 1; j <= i; j++)
+                    {
+                        rez += a[i].ToString().ToLower();
+                    }
+                }
+                return rez;
+            }
+            //26
+
+            public static int Mxdiflg(string[] a1, string[] a2)
+            {
+                if (a1.Length == 0 || a2.Length == 0)
+                    return -1;
+                else
+                {
+                    int max = Math.Abs(a1[0].Length - a2[0].Length);
+                    foreach (string str1 in a1)
+                    {
+                        foreach (string str2 in a2)
+                        {
+                            if (max < Math.Abs(str1.Length - str2.Length))
+                                max = Math.Abs(str1.Length - str2.Length);
+                        }
+                    }
+                    return max;
+                }
+            }
+            //27
+
+            public static string[][] Partlist(string[] arr)
+            {
+                return Enumerable.Range(1, arr.Length - 1)
+                                 .Select(x =>
+                                     new string[]{
+                                 string.Join(" ",arr.Take(x)),
+                                 string.Join(" ",arr.Skip(x).Take(arr.Length-x))
+                                       })
+                                 .ToArray();
+
+            }
+            //28
+
+            public static string VertMirror(string strng)
+            {
+                return string.Join("\n", strng.Split('\n').Select(i => string.Concat(i.Reverse())));
+            }
+            public static string HorMirror(string strng)
+            {
+                return string.Join("\n", strng.Split('\n').Reverse());
+            }
+            public static string Oper(Func<string, string> fct, string s)
+            {
+                return fct(s);
+            }
+            //29
+
+            public static long MaxRot(long n)
+            {
+                long max = n;
+                string number = n.ToString();
+                for (int i = 0; i < number.Length - 1; i++)
+                {
+                    number = number.Substring(0, i) + number.Substring(i + 1) + number[i];
+                    n = long.Parse(number);
+                    if (n > max) max = n;
+                }
+                return max;
+            }
+            //30
+
+            public static int NbDig(int n, int d)
+            {
+                var result = d == 0 ? 1 : 0;
+                for (var k = 1; k <= n; ++k)
+                {
+                    for (var x = k * k; x != 0; x /= 10)
+                    {
+                        if (x % 10 == d)
+                        {
+                            ++result;
+                        }
+                    }
+                }
+                return result;
+            }
+            //31
         }
     }
 }
