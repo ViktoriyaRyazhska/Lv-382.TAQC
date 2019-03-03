14011a8ca2a03c021c4212c1b2c468664e3e0574
Roman Pihel <romanpihel1992@gmail.com>
2019-03-03 20:45:53 +0200

    add 3 tasks

---
+++ AddTasks/Array1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array1
+{
+    class Array1
+    {
+        
+            public static int Century()
+            {
+                Console.Write("Enter year: ");
+                int z;
+                int x = int.Parse(Console.ReadLine());
+                if (x > 0)
+                {
+                    double y = (x / 100.00d);
+                    if ((y % 1) > 0.00d)
+                    {
+                        z = (int)(y + 1);
+                        Console.WriteLine("Century: " + z);
+                        return z;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Century: " + y);
+                        return (int)y;
+                    }
+                }
+                Console.WriteLine("Enter year > 0");
+                return 0;
+            }
+        
+
+    }
+}
---
+++ AddTasks/Array3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array1
+{
+    class Array3
+    {
+        public static void Sum30(int b)
+        {
+            int n = 30;
+            double sum = 0d;
+            if (b >= 0)
+            {
+                double[] arr = new double[n];
+                for (int i = 0; i < n; i++)
+                {
+                    int a;
+                    if (i % 2 == 1)
+                    {
+                        a = i;
+                    }
+                    else
+                    {
+                        a = i / 2;
+                    }
+                    arr[i] = Math.Pow((a - b), 2);
+                    sum += arr[i];
+                }
+                Console.WriteLine("sum = " + sum);
+            }
+            else
+            {
+                Console.WriteLine("Enter b  >= 0");
+            }
+        }
+    }
+}
