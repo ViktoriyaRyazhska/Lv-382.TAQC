1d48ec79a53ed7223e7e0482bf3ba76cfaa3ffeb
Valentin Lobanov <vdlobanovs@gmail.com>
2019-03-04 16:08:03 +0200

    All array tasks

--- Codewar/Codewars/TasksFromDiffSourses/Array Tasks/Arrays.cs
+++ Codewar/Codewars/TasksFromDiffSourses/Array Tasks/Arrays.cs
@@ -3,8 +3,11 @@ using System.Linq;
 
 namespace Codewars
 {
+
     static class Arrays
     {
+        const int aX = 0;
+        const int aY = 0;
         public static void Task1(int year)
         {
             Console.WriteLine(((year - 1) / 100) + 1);
@@ -100,6 +103,105 @@ namespace Codewars
                 a[i] = (39d - i) + Math.Cos(a[i-1]);
             }
             Console.WriteLine(a.Last());
+        }
+        public static int Task10(int[] a, int r)
+        {
+            int counter = 0;
+            int Lenght = a.Length;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (Distance(a[i], a[Lenght - i]) <= r)
+                    counter++;
+            }
+            return counter;
+        }
+        private static double Distance(int bX, int bY)
+        {
+            return Math.Sqrt(Math.Pow((bX-aX),2)+Math.Pow((aY-aX),2));
+        }
+        public static void Task11()
+        {
+            Console.WriteLine("Enter n: ");
+            int n = int.Parse(Console.ReadLine());
+            double[] arr = new double[n];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Enter t[" + (i + 1) + "]: ");
+                arr[i] = double.Parse(Console.ReadLine());
+            }
+            double sum = 0;
+            double ci = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > ci)
+                {
+                    ci = arr[i];
+
+                }
+                sum += arr[i];
+                Console.WriteLine("t ci " + sum);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("t Max= " + ci);
+        }
+        public static void Task12()
+        {
+            Console.WriteLine("Enter n judgies: ");
+            int n = int.Parse(Console.ReadLine());
+            double[] arr = new double[n];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Enter mark j[" + (i + 1) + "]: ");
+                arr[i] = double.Parse(Console.ReadLine());
+            }
+            Array.Sort(arr);
+
+            double sum = 0;
+            for (int i = 1; i < arr.Length - 1; i++)
+            {
+                sum += arr[i];
+            }
+            sum /= (arr.Length - 2);
+            Console.WriteLine("Average mark is: " + sum);
+        }
+        public static void Task13()
+        {
+            Console.WriteLine("Enter natural number: ");
+            string str = Console.ReadLine();
+            int[] arr = new int[str.Length];
+
+            string str2 = "";
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                arr[i] = int.Parse(str.Substring(i, 1));
+                arr[i] = arr[i] * arr[i];
+                str2 += arr[i];
+            }
+            Console.WriteLine("Rezult number is: " + str2);
+        }
+        public static void Task14()
+        {
+            Console.WriteLine("Enter string: ");
+            string str = Console.ReadLine();
+            Console.WriteLine("Number distinct symbols is: " + str.ToCharArray().Distinct().Count());
+
+        }
+        public static void Task16()
+        {
+            Console.WriteLine("Enter array length >= 4: ");
+            int n = int.Parse(Console.ReadLine());
+            double[] arr = new double[n];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Enter element[" + (i + 1) + "]: ");
+                arr[i] = double.Parse(Console.ReadLine());
+            }
+            Array.Sort(arr);
+
+            double sum = arr[0] + arr[1];
+
+            Console.WriteLine("Sum 2 Lovest Elements is: " + sum);
         }
 
         public static int Task16(int[] arr)
