a10dab6cf1a7c126fe89a58ef147212db395758c
bebebe <hahaha@gmail.com>
2019-03-12 18:05:54 +0200

    TestRunner. Example

--- OpenCartTestProject/SimpleTest.cs
+++ OpenCartTestProject/SimpleTest.cs
@@ -4,6 +4,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
 
 namespace OpenCartTestProject
 {
@@ -12,7 +14,7 @@ namespace OpenCartTestProject
     public class SimpleTest
     {
         //[TestMethod]
-        [Test]
+        //[Test]
         public void CheckLogin()
         {
             // Precondition
@@ -69,6 +71,50 @@ namespace OpenCartTestProject
             driver.FindElement(By.Id("password")).Clear();
             driver.FindElement(By.Id("password")).SendKeys("qwerty");
             Thread.Sleep(2000); // For Presentation ONLY
+            //
+            driver.Quit();
+        }
+
+        //[Test]
+        public void CheckScript()
+        {
+            // Precondition
+            IWebDriver driver = new ChromeDriver();
+            //IWebDriver driver = new FirefoxDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20); // by default 0
+            //driver.Navigate().GoToUrl("https://softserve.academy/");
+            driver.Navigate().GoToUrl("https://www.seleniumhq.org/download/");
+            Thread.Sleep(2000); // For Presentation ONLY
+            //
+            // MoveToElement
+            //Actions action = new Actions(driver);
+            //action.MoveToElement(driver.FindElement(By.CssSelector("a[name='side_plugins'] h3"))).Perform();
+            //action.MoveToElement(driver.FindElement(By.LinkText("Useful information"))).Perform();
+            //action.MoveToElement(driver.FindElement(By.Id("instance-2797-header"))).Perform();
+            //
+            // Use JavaScript Injection
+            //IWebElement clock = driver.FindElement(By.Id("instance-2797-header"));
+            IWebElement clock = driver.FindElement(By.CssSelector("a[name='side_plugins'] h3"));
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            //
+            // Search WebElements
+            //IWebElement clock = (IWebElement)js
+            //    .ExecuteScript("return document.getElementsByName('side_plugins')[0];", new object[1] { "" });
+            //Console.WriteLine("side_plugins TEXT: " + clock.Text);
+            //
+            // MoveToElement
+            js.ExecuteScript("arguments[0].scrollIntoView(true);", clock);
+            Thread.Sleep(2000);
+            //
+            //js.ExecuteScript("alert('Hello');");
+            //Thread.Sleep(4000);
+            //driver.SwitchTo().Alert().Accept();
+            //
+            // TakesScreenshot
+            ITakesScreenshot takesScreenshot = driver as ITakesScreenshot;
+            Screenshot screenshot = takesScreenshot.GetScreenshot();
+            screenshot.SaveAsFile("d:/Screenshot1.png", ScreenshotImageFormat.Png);
+            Thread.Sleep(4000); // For Presentation ONLY
             //
             driver.Quit();
         }
