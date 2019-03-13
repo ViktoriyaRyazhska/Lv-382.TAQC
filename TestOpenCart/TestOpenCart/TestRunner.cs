653e5aa00acdc1438e1236fa3793ce17cde15747
Natalia Voloshyna <nataliya.voloshyna1908@gmail.com>
2019-03-13 12:23:56 +0200

    tests for wish list

---
+++ TestOpenCart/TestOpenCart/TestRunner.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+
+namespace TestOpenCart
+{
+    public abstract class TestRunner
+    {
+        protected IWebDriver driver;
+        static readonly string adminName = Environment.GetEnvironmentVariable("adminName", EnvironmentVariableTarget.User).ToString();
+        static readonly string adminPass = Environment.GetEnvironmentVariable("adminPass", EnvironmentVariableTarget.User).ToString();
+
+
+        [OneTimeSetUp]
+        public void BeforeAllMethods()
+        {
+            driver = new FirefoxDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20); // by default 0
+            driver.Navigate().GoToUrl("http://192.168.79.128/opencart/upload/");
+            Thread.Sleep(2000);
+            driver.FindElement(By.CssSelector("#top-links > ul > li.dropdown > a")).Click();
+            driver.FindElement(By.LinkText("Login")).Click();
+            driver.FindElement(By.Name("email")).SendKeys(adminName);
+            driver.FindElement(By.Name("password")).SendKeys(adminPass + OpenQA.Selenium.Keys.Enter);
+            Thread.Sleep(2000);
+        }
+
+        [OneTimeTearDown]
+        public void AfterAllMethods()
+        {
+            driver.Quit();
+        }
+
+        //[SetUp]
+        //public void SetUp()
+        //{
+        //    driver.Navigate().GoToUrl("http://192.168.79.128/opencart/upload/");
+        //}
+
+        [TearDown]
+        //public void TearDown(ITestResult testResult)
+        public void TearDown()
+        {
+            Console.WriteLine("TestContext.CurrentContext.Result = " + NUnit.Framework.TestContext.CurrentContext.Result.Message);
+            if (NUnit.Framework.TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
+            {
+                Console.WriteLine("TestContext.CurrentContext.Result.StackTrace = " + NUnit.Framework.TestContext.CurrentContext.Result.StackTrace);
+                TakesScreenshot("d:/Screenshot12.png");
+                TakesSources("");
+            }
+        }
+        protected void TakesScreenshot(string filePath)
+        {
+            ITakesScreenshot takesScreenshot = driver as ITakesScreenshot;
+            Screenshot screenshot = takesScreenshot.GetScreenshot();
+            screenshot.SaveAsFile(filePath, ScreenshotImageFormat.Png);
+        }
+
+        protected void TakesSources(string filePath)
+        {
+
+        }
+    }
+}
--- TestOpenCart/TestOpenCart/WishListTest.cs
+++ TestOpenCart/TestOpenCart/WishListTest.cs
@@ -1,60 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Windows.Forms;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
+using System.Linq;
 
-namespace UnitTestProjectLv382a
+namespace TestOpenCart
 {
     [TestFixture]
-    public class WishListRegisterUserTest
+    public class WishListRegisterUserTest : TestRunner
     {
-        private IWebDriver driver;
-        static readonly string adminName = Environment.GetEnvironmentVariable("adminName", EnvironmentVariableTarget.User).ToString();
-        static readonly string adminPass = Environment.GetEnvironmentVariable("adminPass", EnvironmentVariableTarget.User).ToString();
-
-        [OneTimeSetUp]
-        public void BeforeAllMethods()
-        {
-            driver = new FirefoxDriver();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            driver.Navigate().GoToUrl("http://192.168.79.128/opencart/upload/");
-            Thread.Sleep(2000);
-            driver.FindElement(By.CssSelector("#top-links > ul > li.dropdown > a")).Click();
-            driver.FindElement(By.LinkText("Login")).Click();
-            driver.FindElement(By.Name("email")).SendKeys(adminName);
-            driver.FindElement(By.Name("password")).SendKeys(adminPass + OpenQA.Selenium.Keys.Enter);
-            Thread.Sleep(2000);
-        }
-
-        [OneTimeTearDown]
-        public void AfterAllMethods()
-        {
-            driver.Quit();
-        }       
+      
         //DataProvider
         private static readonly object[] Choose =
-        {  new object[] { "//div[contains(@class, 'product-layout')]//a[contains(text(), 'iPhone')]/../../following-sibling::div/button[@data-original-title='Add to Wish List']", "iPhone", "table.table.table-bordered.table-hover td.text-left a[href*='/product&product_id=40']"},
-           new object[] { "//div[contains(@class, 'product-layout')]//a[contains(text(), 'MacBook')]/../../following-sibling::div/button[@data-original-title='Add to Wish List']", "MacBook", "table.table.table-bordered.table-hover td.text-left a[href*='/product&product_id=43']"},
-           new object[] { "//div[contains(@class, 'product-layout')]//a[contains(text(), 'Canon EOS 5D')]/../../following-sibling::div/button[@data-original-title='Add to Wish List']", "Canon EOS 5D", "table.table.table-bordered.table-hover td.text-left a[href*='/product&product_id=30']"},
+        {
+           new List<string>() { "Canon EOS 5D", "iPhone", "MacBook" }
         };
-
-        //[Test, TestCaseSource("Choose")]
-        public void CheckRegisteredUserAddFromFeaturedTab(string itemAddToWish, string nameInWish, string itemInWish)
+        [Test, TestCaseSource("Choose")]
+        public void CheckRegisteredUserAddFromFeaturedTab(List<string> expected)
         {
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);           
             driver.FindElement(By.XPath("//i[@class = 'fa fa-home']")).Click();            
-            driver.FindElement(By.XPath(itemAddToWish)).Click();
+            driver.FindElement(By.XPath("//div[contains(@class, 'product-layout')]//a[contains(text(), 'iPhone')]/../../following-sibling::div/button[@data-original-title='Add to Wish List']")).Click();
+            driver.FindElement(By.XPath("//div[contains(@class, 'product-layout')]//a[contains(text(), 'MacBook')]/../../following-sibling::div/button[@data-original-title='Add to Wish List']")).Click();
+            driver.FindElement(By.XPath("//div[contains(@class, 'product-layout')]//a[contains(text(), 'Canon EOS 5D')]/../../following-sibling::div/button[@data-original-title='Add to Wish List']")).Click();
             Thread.Sleep(2000);
             driver.FindElement(By.CssSelector("#wishlist-total > i")).Click();
             Thread.Sleep(2000);
-            string actual = driver.FindElement(By.CssSelector(itemInWish)).Text;
-            string expected = nameInWish;
-            NUnit.Framework.Assert.AreEqual(expected, actual, $"In Wish List are no expected item: {expected}");
+            List<string> actual = driver.FindElements(By.CssSelector("#content > div.table-responsive td.text-left > a")).ToList<IWebElement>().Select(x => x.Text).ToList();
+            Assert.AreEqual(expected, actual, $"In Wish List are no expected item: {expected}");
             Thread.Sleep(2000);
+            driver.FindElement(By.CssSelector("td[class = 'text-right']>a[href*= '/wishlist&remove=40']")).Click();
+            driver.FindElement(By.CssSelector("td[class = 'text-right']>a[href*= '/wishlist&remove=30']")).Click();
+            driver.FindElement(By.CssSelector("td[class = 'text-right']>a[href*= '/wishlist&remove=43']")).Click();
+
         }
 
         [Test]
@@ -114,27 +96,15 @@ namespace UnitTestProjectLv382a
     }
 
     [TestFixture]
-    public class WishListNotRegisterUserTest
+    public class WishListNotRegisterUserTest : TestRunner
     {
-        private IWebDriver driver;
 
-        [OneTimeSetUp]
-        public void BeforeAllMethods()
-        {
-            driver = new FirefoxDriver();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-            driver.Navigate().GoToUrl("http://192.168.79.128/opencart/upload/");
-        }
-
-        [OneTimeTearDown]
-        public void AfterAllMethods()
-        {
-            driver.Quit();
-        }
-       
         [Test]
         public void CheckNotRegisteredUser()
         {
+            driver.FindElement(By.CssSelector("#top-links > ul > li.dropdown > a")).Click();
+            driver.FindElement(By.CssSelector("ul.dropdown-menu.dropdown-menu-right a[href*='route=account/logout']")).Click();
+            driver.FindElement(By.XPath("//i[@class = 'fa fa-home']")).Click();
             driver.FindElement(By.XPath("//div[contains(@class, 'product-layout')]//a[contains(text(), 'iPhone')]/../../following-sibling::div/button[@data-original-title='Add to Wish List']")).Click();
             Thread.Sleep(2000);
             string actual = driver.FindElement(By.CssSelector("div.alert.alert-success a[href*='route=account/login']")).Text;
