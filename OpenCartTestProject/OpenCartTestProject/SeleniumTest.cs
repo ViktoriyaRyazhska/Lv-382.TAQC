d8e2fa0d7d61603f8ecea3037cbc7d630d015195
SerhiiZakharchuk <serhiizakharchuk@gmail.com>
2019-03-13 18:08:31 +0200

    add testRuner

--- OpenCartTestProject/OpenCartTestProject/SeleniumTest.cs
+++ OpenCartTestProject/OpenCartTestProject/SeleniumTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using NUnit.Framework;
+using OpenCartTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
@@ -9,48 +10,22 @@ namespace OpenCartTestProject
 {
         [TestFixture]
         //[Parallelizable(ParallelScope.All)]
-        public class SeleniumTest
+        public class SeleniumTest : TestRuner
         {
-            private IWebDriver driver;
-            private string urlForAdminPage = "http://192.168.85.129/opencart/upload/admin";
-            private string urlForMainPage = "http://192.168.85.129/opencart/upload";
-            static readonly string adminLogin = Environment.GetEnvironmentVariable("adminLogin");
-            static readonly string adminPassword = Environment.GetEnvironmentVariable("adminPassword");
-            private string tokenHttp;
-
-            [OneTimeSetUp]
-            public void BeforeAllMethods()
-            {
-                driver = new ChromeDriver();
-                //driver = new FirefoxDriver();
-                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
-                driver.Navigate().GoToUrl(urlForAdminPage);
-                driver.FindElement(By.Id("input-username")).SendKeys(adminLogin);
-                driver.FindElement(By.Id("input-password")).SendKeys(adminPassword);
-                driver.FindElement(By.CssSelector("#content > div > div > div > div > div.panel-body > form > div.text-right > button")).Click();
-                tokenHttp = driver.Url;
-            }
-
-            [OneTimeTearDown]
-            public void AfterAllMethods()
-            {
-                driver.Quit();
-            }
-
             [Test]
             public void AddNewMenuItem()
             {
                 driver.Navigate().GoToUrl(tokenHttp);
-                driver.FindElement(By.CssSelector("#menu-catalog > a > i")).Click();
-                driver.FindElement(By.CssSelector("#menu-catalog > ul > li:nth-child(1) > a")).Click();
-                driver.FindElement(By.CssSelector("#content > div.page-header > div > div > a.btn.btn-primary > i")).Click();
+                driver.FindElement(By.Id("menu-catalog")).Click();
+                driver.FindElement(By.CssSelector("#menu-catalog a[href*='category']")).Click();
+                driver.FindElement(By.CssSelector(".page-header a.btn.btn-primary")).Click();
                 driver.FindElement(By.Id("input-name1")).SendKeys("Test1");
                 driver.FindElement(By.Id("input-meta-title1")).SendKeys("Test1");
-                driver.FindElement(By.CssSelector("#form-category > ul > li:nth-child(2) > a")).Click();
+                driver.FindElement(By.CssSelector("a[href*='data']")).Click();
                 driver.FindElement(By.Id("input-top")).Click();
                 driver.FindElement(By.Id("input-sort-order")).SendKeys("9");
-                driver.FindElement(By.CssSelector("#content > div.page-header > div > div > button")).Click();
-                driver.FindElement(By.CssSelector("#content > div.page-header > div > div > a.btn.btn-default")).Click();
+                driver.FindElement(By.CssSelector("div.pull-right .btn.btn-primary")).Click();
+                driver.FindElement(By.CssSelector(".btn.btn-default")).Click();
 
                 driver.Navigate().GoToUrl(urlForMainPage);
                 //Thread.Sleep(3000);   //For presentation only
@@ -59,12 +34,13 @@ namespace OpenCartTestProject
             public void AddNewMenuItem1()
             {
                 driver.Navigate().GoToUrl(tokenHttp);
-                driver.FindElement(By.CssSelector("#menu-catalog > a > i")).Click();
-                driver.FindElement(By.CssSelector("#menu-catalog > ul > li:nth-child(1) > a")).Click();
-                driver.FindElement(By.CssSelector("#form-category > div > table > thead > tr > td.text-left > a")).Click();
+                driver.FindElement(By.Id("menu-catalog")).Click();
+                driver.FindElement(By.CssSelector("#menu-catalog a[href*='category']")).Click();
+                driver.FindElement(By.CssSelector("a.asc")).Click();
                 driver.FindElement(By.CssSelector("#form-category > div > table > tbody > tr:nth-child(1) > td.text-center > input[type='checkbox']")).Click();
-                driver.FindElement(By.CssSelector("#content > div.page-header > div > div > button")).Click();
+                driver.FindElement(By.CssSelector(".btn.btn-danger")).Click();
                 driver.SwitchTo().Alert().Accept();
+                driver.FindElement(By.CssSelector(".btn.btn-default")).Click();
 
                 driver.Navigate().GoToUrl(urlForMainPage);
                 //Thread.Sleep(3000);   //For presentation only
@@ -73,26 +49,27 @@ namespace OpenCartTestProject
 
             //DataProvider
             private static readonly object[] ConverterData =
-            {  new object[] {"ear1", "ear1", 1},
-           new object[] {"ear2", "ear2", 2},
-           new object[] {"ear3", "ear3", 3}
-        };
+            {
+            new object[] {"ear1", "ear1", 1},
+            new object[] {"ear2", "ear2", 2},
+            new object[] {"ear3", "ear3", 3}
+            };
             [Test, TestCaseSource("ConverterData")]
             public void AddNewMenuSubItem(string n, string t, int o)
             {
                 driver.Navigate().GoToUrl(tokenHttp);
-                driver.FindElement(By.CssSelector("#menu-catalog > a > i")).Click();
-                driver.FindElement(By.CssSelector("#menu-catalog > ul > li:nth-child(1) > a")).Click();
-                driver.FindElement(By.CssSelector("#content > div.page-header > div > div > a.btn.btn-primary > i")).Click();
+                driver.FindElement(By.Id("menu-catalog")).Click();
+                driver.FindElement(By.CssSelector("#menu-catalog a[href*='category']")).Click();
+                driver.FindElement(By.CssSelector(".page-header a.btn.btn-primary")).Click();
                 driver.FindElement(By.Id("input-name1")).SendKeys(n);
                 driver.FindElement(By.Id("input-meta-title1")).SendKeys(t);
-                driver.FindElement(By.CssSelector("#form-category > ul > li:nth-child(2) > a")).Click();
-                driver.FindElement(By.Id("input-parent")).SendKeys("Ea");
+                driver.FindElement(By.CssSelector("a[href*='data']")).Click();
+                driver.FindElement(By.Id("input-parent")).SendKeys("Earphones");
                 driver.FindElement(By.CssSelector("#tab-data > div:nth-child(1) > div > ul > li:nth-child(2) > a")).Click();
                 driver.FindElement(By.Id("input-sort-order")).Clear();
                 driver.FindElement(By.Id("input-sort-order")).SendKeys(o.ToString());
-                driver.FindElement(By.CssSelector("#content > div.page-header > div > div > button")).Click();
-                driver.FindElement(By.CssSelector("#content > div.page-header > div > div > a.btn.btn-default")).Click();
+                driver.FindElement(By.CssSelector("div.pull-right .btn.btn-primary")).Click();
+                driver.FindElement(By.CssSelector(".btn.btn-default")).Click();
 
                 driver.Navigate().GoToUrl(urlForMainPage);
                 //Thread.Sleep(3000);   //For presentation only
@@ -103,30 +80,30 @@ namespace OpenCartTestProject
             public void ReplaceMenuItem()
             {
                 driver.Navigate().GoToUrl(tokenHttp);
-                driver.FindElement(By.CssSelector("#menu-catalog > a > i")).Click();
-                driver.FindElement(By.CssSelector("#menu-catalog > ul > li:nth-child(1) > a")).Click();
+                driver.FindElement(By.Id("menu-catalog")).Click();
+                driver.FindElement(By.CssSelector("#menu-catalog a[href*='category']")).Click();
                 driver.FindElement(By.CssSelector("#form-category > div > table > tbody > tr:nth-child(1) > td:nth-child(4) > a")).Click();
-                driver.FindElement(By.CssSelector("#form-category > ul > li:nth-child(2) > a")).Click();
+                driver.FindElement(By.CssSelector("a[href*='data']")).Click();
                 driver.FindElement(By.Id("input-sort-order")).Clear();
                 driver.FindElement(By.Id("input-sort-order")).SendKeys("0");
-                driver.FindElement(By.CssSelector("#content > div.page-header > div > div > button")).Click();
-                driver.FindElement(By.CssSelector("#content > div.page-header > div > div > a.btn.btn-default")).Click();
+                driver.FindElement(By.CssSelector("div.pull-right .btn.btn-primary")).Click();
+                driver.FindElement(By.CssSelector(".btn.btn-default")).Click();
 
-                driver.Navigate().GoToUrl(urlForMainPage);
+            driver.Navigate().GoToUrl(urlForMainPage);
                 //Thread.Sleep(3000);   //For presentation only
             }
             [Test]
             public void ReplaceMenuItem1()
             {
                 driver.Navigate().GoToUrl(tokenHttp);
-                driver.FindElement(By.CssSelector("#menu-catalog > a > i")).Click();
-                driver.FindElement(By.CssSelector("#menu-catalog > ul > li:nth-child(1) > a")).Click();
+                driver.FindElement(By.Id("menu-catalog")).Click();
+                driver.FindElement(By.CssSelector("#menu-catalog a[href*='category']")).Click();
                 driver.FindElement(By.CssSelector("#form-category > div > table > tbody > tr:nth-child(1) > td:nth-child(4) > a")).Click();
-                driver.FindElement(By.CssSelector("#form-category > ul > li:nth-child(2) > a")).Click();
+                driver.FindElement(By.CssSelector("a[href*='data']")).Click();
                 driver.FindElement(By.Id("input-sort-order")).Clear();
                 driver.FindElement(By.Id("input-sort-order")).SendKeys("6");
-                driver.FindElement(By.CssSelector("#content > div.page-header > div > div > button")).Click();
-                driver.FindElement(By.CssSelector("#content > div.page-header > div > div > a.btn.btn-default")).Click();
+                driver.FindElement(By.CssSelector("div.pull-right .btn.btn-primary")).Click();
+                driver.FindElement(By.CssSelector(".btn.btn-default")).Click();
 
                 driver.Navigate().GoToUrl(urlForMainPage);
                 //Thread.Sleep(3000);   //For presentation only
@@ -137,14 +114,14 @@ namespace OpenCartTestProject
             public void ReplaceMenuSubItem()
             {
                 driver.Navigate().GoToUrl(tokenHttp);
-                driver.FindElement(By.CssSelector("#menu-catalog > a > i")).Click();
-                driver.FindElement(By.CssSelector("#menu-catalog > ul > li:nth-child(1) > a")).Click();
+                driver.FindElement(By.Id("menu-catalog")).Click();
+                driver.FindElement(By.CssSelector("#menu-catalog a[href*='category']")).Click();
                 driver.FindElement(By.CssSelector("#form-category > div > table > tbody > tr:nth-child(3) > td:nth-child(4) > a")).Click();
-                driver.FindElement(By.CssSelector("#form-category > ul > li:nth-child(2) > a")).Click();
+                driver.FindElement(By.CssSelector("a[href*='data']")).Click();
                 driver.FindElement(By.Id("input-sort-order")).Clear();
                 driver.FindElement(By.Id("input-sort-order")).SendKeys("5");
-                driver.FindElement(By.CssSelector("#content > div.page-header > div > div > button")).Click();
-                driver.FindElement(By.CssSelector("#content > div.page-header > div > div > a.btn.btn-default")).Click();
+                driver.FindElement(By.CssSelector("div.pull-right .btn.btn-primary")).Click();
+                driver.FindElement(By.CssSelector(".btn.btn-default")).Click();
 
                 driver.Navigate().GoToUrl(urlForMainPage);
                 //Thread.Sleep(3000);   //For presentation only
@@ -153,14 +130,14 @@ namespace OpenCartTestProject
             public void ReplaceMenuSubItem1()
             {
                 driver.Navigate().GoToUrl(tokenHttp);
-                driver.FindElement(By.CssSelector("#menu-catalog > a > i")).Click();
-                driver.FindElement(By.CssSelector("#menu-catalog > ul > li:nth-child(1) > a")).Click();
+                driver.FindElement(By.Id("menu-catalog")).Click();
+                driver.FindElement(By.CssSelector("#menu-catalog a[href*='category']")).Click();
                 driver.FindElement(By.CssSelector("#form-category > div > table > tbody > tr:nth-child(3) > td:nth-child(4) > a")).Click();
-                driver.FindElement(By.CssSelector("#form-category > ul > li:nth-child(2) > a")).Click();
+                driver.FindElement(By.CssSelector("a[href*='data']")).Click();
                 driver.FindElement(By.Id("input-sort-order")).Clear();
                 driver.FindElement(By.Id("input-sort-order")).SendKeys("1");
-                driver.FindElement(By.CssSelector("#content > div.page-header > div > div > button")).Click();
-                driver.FindElement(By.CssSelector("#content > div.page-header > div > div > a.btn.btn-default")).Click();
+                driver.FindElement(By.CssSelector("div.pull-right .btn.btn-primary")).Click();
+                driver.FindElement(By.CssSelector(".btn.btn-default")).Click();
 
                 driver.Navigate().GoToUrl(urlForMainPage);
                 //Thread.Sleep(3000);   //For presentation only
---
+++ OpenCartTestProject/OpenCartTestProject/TestRuner.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCartTesting
+{
+    abstract public class TestRuner
+    {
+        protected IWebDriver driver;
+        protected string urlForAdminPage = "http://192.168.85.129/opencart/upload/admin";
+        protected string urlForMainPage = "http://192.168.85.129/opencart/upload";
+        protected string tokenHttp;
+
+        [OneTimeSetUp]
+        public void BeforeAllMethods()
+        {
+            driver = new ChromeDriver();
+            //driver = new FirefoxDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.Navigate().GoToUrl(urlForAdminPage);
+            driver.FindElement(By.Id("input-username")).SendKeys(Environment.GetEnvironmentVariable("adminLogin"));
+            driver.FindElement(By.Id("input-password")).SendKeys(Environment.GetEnvironmentVariable("adminPassword"));
+            driver.FindElement(By.CssSelector(".btn.btn-primary")).Click();
+            tokenHttp = driver.Url;
+        }
+
+        [OneTimeTearDown]
+        public void AfterAllMethods()
+        {
+            driver.Quit();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            //driver.Navigate().GoToUrl(tokenHttp);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Console.WriteLine("TestContext.CurrentContext.Result = " + TestContext.CurrentContext.Result.Message);
+
+            if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
+            {
+                File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + "../../ErrorLogs/Log.txt",
+                    "TestContext.CurrentContext.Result.StackTrace = " + TestContext.CurrentContext.Result.StackTrace + "\n");
+                string screenshot = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+                TakesScreenshot(AppDomain.CurrentDomain.BaseDirectory + "../../ErrorLogs/Screenshots/" + screenshot + ".png");
+            }
+
+            //driver.Manage().Cookies.DeleteAllCookies();
+        }
+
+        protected void TakesScreenshot(string filePath)
+        {
+            ITakesScreenshot takesScreenshot = driver as ITakesScreenshot;
+            Screenshot screenshot = takesScreenshot.GetScreenshot();
+            screenshot.SaveAsFile(filePath, ScreenshotImageFormat.Png);
+        }
+    }
+}
