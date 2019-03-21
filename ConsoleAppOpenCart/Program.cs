8db66090d373fcbfedaf31a14b971c4de1167eb4
bebebe <hahaha@gmail.com>
2019-03-21 20:35:57 +0200

    add static factory

---
+++ ConsoleAppOpenCart/Program.cs
@@ -0,0 +1,79 @@
+﻿using OpenCartTestProject.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppOpenCart
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // 1. Use Constructor
+            //User user = new User("firstname1", "lastname1",
+            //    "email1", "telephone1", "fax1",
+            //    "company1", "1address1", "1address2",
+            //    "city1", "postcode1", "country1",
+            //    "region1", "password1", true);
+            //Console.WriteLine("user.Lastname" + user.Lastname);
+            //
+            // 2. Default Constructor and Setters
+            //User user = new User();
+            //user.SetFirstname("firstname2");
+            //user.SetLastname("lastname2");
+            //user.SetEmail("email2");
+            //user.SetTelephone("telephone2");
+            //user.SetFax("fax2");
+            //user.SetCompany("company2");
+            //user.SetAddress1("2address1");
+            //user.SetAddress2("2address2");
+            //user.SetCity("city2");
+            //user.SetPostcode("postcode2");
+            //user.SetCountry("country2");
+            //user.SetRegionState("regionState2");
+            //user.SetPassword("password2");
+            //user.SetSubscribe(true);
+            //Console.WriteLine("user.Lastname" + user.Lastname);
+            //
+            // 3. Add Fluent Interface
+            //User user = new User()
+            //    .SetFirstname("firstname3")
+            //    .SetLastname("lastname3")
+            //    .SetEmail("email3")
+            //    .SetTelephone("telephone3")
+            //    .SetFax("fax3")
+            //    .SetCompany("company3")
+            //    .SetAddress1("3address1")
+            //    .SetAddress2("3address2")
+            //    .SetCity("city3")
+            //    .SetPostcode("postcode3")
+            //    .SetCountry("country3")
+            //    .SetRegionState("regionState3")
+            //    .SetPassword("password3")
+            //    .SetSubscribe(true);
+            //Console.WriteLine("user.Lastname" + user.Lastname);
+            //
+            // 4. Add Static Factory
+            User user = User.Get()
+                .SetFirstname("firstname4")
+                .SetLastname("lastname4")
+                .SetEmail("email4")
+                .SetTelephone("telephone4")
+                .SetFax("fax4")
+                .SetCompany("company4")
+                .SetAddress1("4address1")
+                .SetAddress2("4address2")
+                .SetCity("city4")
+                .SetPostcode("postcode4")
+                .SetCountry("country4")
+                .SetRegionState("regionState4")
+                .SetPassword("password4")
+                .SetSubscribe(true);
+            Console.WriteLine("user.Lastname" + user.Lastname);
+            //
+            Console.WriteLine("\nDone.");
+        }
+    }
+}
--- OpenCartTestProject/Data/User.cs
+++ OpenCartTestProject/Data/User.cs
@@ -12,9 +12,18 @@ namespace OpenCartTestProject.Data
         public string Lastname { get; private set; }
         public string Email { get; private set; }
         public string Telephone { get; private set; }
+        // 1. Use Constructor
+        //public string Fax { get; set; }         // not required
+        // 2. Default Constructor and Setters
         public string Fax { get; private set; }         // not required
+        // 1. Use Constructor
+        //public string Company { get; set; }     // not required
+        // 2. Default Constructor and Setters
         public string Company { get; private set; }     // not required
         public string Address1 { get; private set; }
+        // 1. Use Constructor
+        //public string Address2 { get; set; }    // not required
+        // 2. Default Constructor and Setters
         public string Address2 { get; private set; }    // not required
         public string City { get; private set; }
         public string Postcode { get; private set; }
@@ -23,27 +32,167 @@ namespace OpenCartTestProject.Data
         public string Password { get; private set; }
         public bool Subscribe { get; private set; }
 
-        public User(string firstname, string lastname,
-                string email, string telephone, string fax,
-                string company, string address1, string address2,
-                string city, string postcode, string country,
-                string region, string password, bool subscribe)
+        // 1. Use Constructor
+        //public User(string firstname, string lastname,
+        //        string email, string telephone, string fax,
+        //        string company, string address1, string address2,
+        //        string city, string postcode, string country,
+        //        string region, string password, bool subscribe)
+        //{
+        //    Firstname = firstname;
+        //    Lastname = lastname;
+        //    Email = email;
+        //    Telephone = telephone;
+        //    Fax = fax;
+        //    Company = company;
+        //    Address1 = address1;
+        //    Address2 = address2;
+        //    City = city;
+        //    Postcode = postcode;
+        //    Country = country;
+        //    RegionState = region;
+        //    Password = password;
+        //    Subscribe = subscribe;
+        //}
+
+        //public User(string firstname, string lastname,
+        //       string email, string telephone,
+        //       string address1,
+        //       string city, string postcode, string country,
+        //       string region, string password, bool subscribe)
+        //{
+        //    Firstname = firstname;
+        //    Lastname = lastname;
+        //    Email = email;
+        //    Telephone = telephone;
+        //    Fax = string.Empty;
+        //    Company = string.Empty;
+        //    Address1 = address1;
+        //    Address2 = string.Empty;
+        //    City = city;
+        //    Postcode = postcode;
+        //    Country = country;
+        //    RegionState = region;
+        //    Password = password;
+        //    Subscribe = subscribe;
+        //}
+
+        // 2. Default Constructor and Setters
+        //public User()
+        // 4. Add Static Factory
+        private User()
+        {
+            Firstname = string.Empty;
+            Lastname = string.Empty;
+            Email = string.Empty;
+            Telephone = string.Empty;
+            Fax = string.Empty;
+            Company = string.Empty;
+            Address1 = string.Empty;
+            Address2 = string.Empty;
+            City = string.Empty;
+            Postcode = string.Empty;
+            Country = string.Empty;
+            RegionState = string.Empty;
+            Password = string.Empty;
+            Subscribe = true;
+        }
+
+        // 4. Add Static Factory
+        public static User Get()
+        {
+            return new User();
+        }
+
+        // setters
+
+        // 2. Default Constructor and Setters
+        //public void SetFirstname(string firstname)
+        // 3. Add Fluent Interface
+        public User SetFirstname(string firstname)
         {
             Firstname = firstname;
-            Lastname = lastname;
-            Email = email;
-            Telephone = telephone;
-            Fax = fax;
-            Company = company;
-            Address1 = address1;
-            Address2 = address2;
-            City = city;
-            Postcode = postcode;
-            Country = country;
-            RegionState = region;
-            Password = password;
-            Subscribe = subscribe;
+            return this;
         }
+
+        public User SetLastname(string lastname)
+        {
+            Lastname = lastname;
+            return this;
+        }
+
+        public User SetEmail(string email)
+        {
+            Email = email;
+            return this;
+        }
+
+        public User SetTelephone(string telephone)
+        {
+            Telephone = telephone;
+            return this;
+        }
+
+        public User SetFax(string fax)
+        {
+            Fax = string.Empty;
+            return this;
+        }
+
+        public User SetCompany(string company)
+        {
+            Company = string.Empty;
+            return this;
+        }
+
+        public User SetAddress1(string address1)
+        {
+            Address1 = address1;
+            return this;
+        }
+
+        public User SetAddress2(string address2)
+        {
+            Address2 = string.Empty;
+            return this;
+        }
+
+        public User SetCity(string city)
+        {
+            City = city;
+            return this;
+        }
+
+        public User SetPostcode(string postcode)
+        {
+            Postcode = postcode;
+            return this;
+        }
+
+        public User SetCountry(string country)
+        {
+            Country = country;
+            return this;
+        }
+
+        public User SetRegionState(string regionState)
+        {
+            RegionState = regionState;
+            return this;
+        }
+
+        public User SetPassword(string password)
+        {
+            Password = password;
+            return this;
+        }
+
+        public User SetSubscribe(bool subscribe)
+        {
+            Subscribe = subscribe;
+            return this;
+        }
+
     }
 
 }
