628e48fa57c3f3f1e68fc8bda95f2ce66563ffad
Oleksandra Yarmoliuk <sasha.yarmolyuk@gmail.com>
2019-03-23 17:11:38 +0200

    MARightMenuPart

--- OpenCart Testing/OpenCart Testing/Pages/StaticParts/ARightMenuPart.cs
+++ OpenCart Testing/OpenCart Testing/Pages/StaticParts/ARightMenuPart.cs
@@ -14,7 +14,33 @@ namespace OpenCart_Testing.Pages.StaticParts
         {
         }
 
-        public IWebElement MyAccount
+        public new IWebElement MyAccount
         { get { return driver.FindElement(MARightMenuPart.locatorMyAccount); } }
+        public IWebElement EditAccount
+        { get { return driver.FindElement(MARightMenuPart.locatorEditAccount); } }
+        public IWebElement Password
+        { get { return driver.FindElement(MARightMenuPart.locatorPassword); } }
+        public IWebElement AddressBook
+        { get { return driver.FindElement(MARightMenuPart.locatorAddressBook); } }
+        public new IWebElement WishList
+        { get { return driver.FindElement(MARightMenuPart.locatorWishList); } }
+        public IWebElement OrderHistory
+        { get { return driver.FindElement(MARightMenuPart.locatorOrderHistory); } }
+        public IWebElement Downloads
+        { get { return driver.FindElement(MARightMenuPart.locatorDownloads); } }
+        public IWebElement RecurringPayments
+        { get { return driver.FindElement(MARightMenuPart.locatorRecurringPayments); } }
+        public IWebElement RewardPoints
+        { get { return driver.FindElement(MARightMenuPart.locatorRewardPoints); } }
+        public IWebElement Returns
+        { get { return driver.FindElement(MARightMenuPart.locatorReturns); } }
+        public IWebElement Transactions
+        { get { return driver.FindElement(MARightMenuPart.locatorTransactions); } }
+        public IWebElement Newsletter
+        { get { return driver.FindElement(MARightMenuPart.locatorNewsletter); } }
+        public IWebElement Logout
+        { get { return driver.FindElement(MARightMenuPart.locatorLogout); } }
+
+
     }
 }
--- OpenCart Testing/OpenCart Testing/Pages/UIMapping/MARightMenuPart.cs
+++ OpenCart Testing/OpenCart Testing/Pages/UIMapping/MARightMenuPart.cs
@@ -11,5 +11,17 @@ namespace OpenCart_Testing.Pages.UIMapping
     {
 
         public static By locatorMyAccount => By.XPath("//div[@class='list-group']/a[contains(@href, 'account/account')]");
+        public static By locatorEditAccount => By.XPath("//div[@class='list-group']/a[contains(@href, 'account/edit')]");
+        public static By locatorPassword => By.XPath("//div[@class='list-group']/a[contains(@href, 'account/password')]");
+        public static By locatorAddressBook => By.XPath("//div[@class='list-group']/a[contains(@href, 'account/address')]");
+        public static By locatorWishList => By.XPath("//div[@class='list-group']/a[contains(@href, 'account/wishlist')]");
+        public static By locatorOrderHistory => By.XPath("//div[@class='list-group']/a[contains(@href, 'account/order')]");
+        public static By locatorDownloads => By.XPath("//div[@class='list-group']/a[contains(@href, 'account/download')]");
+        public static By locatorRecurringPayments => By.XPath("//div[@class='list-group']/a[contains(@href, 'account/recurring')]");
+        public static By locatorRewardPoints => By.XPath("//div[@class='list-group']/a[contains(@href, 'account/reward')]");
+        public static By locatorReturns => By.XPath("//div[@class='list-group']/a[contains(@href, 'account/return')]");
+        public static By locatorTransactions => By.XPath("//div[@class='list-group']/a[contains(@href, 'account/transaction')]");
+        public static By locatorNewsletter => By.XPath("//div[@class='list-group']/a[contains(@href, 'account/newsletter')]");
+        public static By locatorLogout => By.XPath("//div[@class='list-group']/a[contains(@href, 'account/logout')]");
     }
 }
