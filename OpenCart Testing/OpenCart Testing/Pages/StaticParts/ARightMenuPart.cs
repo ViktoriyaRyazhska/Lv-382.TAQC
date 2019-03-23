652e33fe4897d7567ffb8c892d199077462560d1
Oleksandra Yarmoliuk <sasha.yarmolyuk@gmail.com>
2019-03-23 16:56:23 +0200

    ARightMenuPart

---
+++ OpenCart Testing/OpenCart Testing/Pages/StaticParts/ARightMenuPart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCart_Testing.Pages.StaticParts
+{
+    public abstract class ARightMenuPart: ABreadCrumbsPart
+    {
+        public ARightMenuPart(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public IWebElement MyAccount
+        { get { return driver.FindElement(MARightMenuPart.locatorMyAccount); } }
+    }
+}
---
+++ OpenCart Testing/OpenCart Testing/Pages/UIMapping/MARightMenuPart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCart_Testing.Pages.UIMapping
+{
+    public static class MARightMenuPart
+    {
+
+        public static By locatorMyAccount => By.XPath("//div[@class='list-group']/a[contains(@href, 'account/account')]");
+    }
+}
