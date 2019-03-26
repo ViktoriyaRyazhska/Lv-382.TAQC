e55b07684ba6751625b6373eb06c9b628455f050
SerhiiZakharchuk <serhiizakharchuk@gmail.com>
2019-03-26 16:05:10 +0200

    add locators

--- OpenCart Testing/OpenCart Testing/Pages/SearchPage/SearchCriteriaPage.cs
+++ OpenCart Testing/OpenCart Testing/Pages/SearchPage/SearchCriteriaPage.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using OpenCart_Testing.Pages.UIMapping;
+using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,14 +10,69 @@ namespace OpenCart_Testing.Pages
 {
     public class SearchCriteriaPage : ASearchResultPart
     {
+        //protected IWebDriver driver;
+
+        public IWebElement SearchItemsCount
+        { get { return driver.FindElement(MSearchCriteriaPage.locatorSearchItemsCount); } }
+        public IWebElement ItemNotMatchesMessage
+        { get { return driver.FindElement(MSearchCriteriaPage.locatorItemNotMatchesMessage); } }
+        public IWebElement SearchAlertMessage
+        { get { return driver.FindElement(MSearchCriteriaPage.locatorSearchAlertMessage); } }
+        public IWebElement GridView
+        { get { return driver.FindElement(MSearchCriteriaPage.locatorGridView); } }
+        public IWebElement ListView
+        { get { return driver.FindElement(MSearchCriteriaPage.locatorListView); } }
+
         public SearchCriteriaPage(IWebDriver driver) : base(driver)
         {
         }
 
         // Page Object
+        
+        //SearchItemsCount
+        public string GetSearchItemsCount()
+        {
+            return SearchItemsCount.Text;
+        }
+        //SearchItemNotMatchesMessage
+        public string GetItemNotMatchesMessage()
+        {
+            return ItemNotMatchesMessage.Text;
+        }
+        //SearchItemNotMatchesMessage
+        public string GetSearchAlertMessage()
+        {
+            return SearchAlertMessage.Text;
+        }
+        //SearchGridViewButton
+        public void ClickGridView()
+        {
+            GridView.Click();
+        }
+        //SearchListViewButton
+        public void ClickListView()
+        {
+            ListView.Click();
+        }
 
         // Functional
+        public int FindActualCount()
+        {
+            return int.Parse(GetSearchItemsCount().Split(' ')[5]);
+        }
 
+        //public bool hasClass(IWebElement element, string searchedClass)
+        //{
+        //    string[] classes = element.GetAttribute("class").Split(' ');
+        //    foreach (string str in classes)
+        //    {
+        //        if (str.Equals(searchedClass))
+        //        {
+        //            return true;
+        //        }
+        //    }
+        //    return false;
+        //}
         // Business Logic
 
     }
