8ddc5e26bc3b1090c7e58c56db8fa7f73589e6cf
SerhiiZakharchuk <serhiizakharchuk@gmail.com>
2019-03-28 11:36:37 +0200

    add json files

--- OpenCart Testing/OpenCart Testing/TestData/SimpleSearchData/SimpleSearchRepository.cs
+++ OpenCart Testing/OpenCart Testing/TestData/SimpleSearchData/SimpleSearchRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenCart_Testing.Tools;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,14 @@ namespace OpenCart_Testing.TestData.SimpleSearchData
 {
     class SimpleSearchRepository
     {
+        //private volatile static SimpleSearchRepository instance;
+        //private static object lockingObject = new object();
+        //private const string reviewEmptyListMessage = "There are no reviews for this product.";
+        private static string directory = "SimpleSearch";
+ 
+        public ISimpleSearch NewSearchDataFromJson(string fileName)
+        {
+            return JsonParser.DeserializeFromFile<SimpleSearch>(directory, fileName) as ISimpleSearch;
+        }
     }
 }
