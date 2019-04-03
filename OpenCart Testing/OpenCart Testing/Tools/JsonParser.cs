1cdb15a1ad27d212cf0aadb0bce871e42dce479c
Oleksandra Yarmoliuk <sasha.yarmolyuk@gmail.com>
2019-04-03 17:28:43 +0300

    changes

--- OpenCart Testing/OpenCart Testing/Tools/JsonParser.cs
+++ OpenCart Testing/OpenCart Testing/Tools/JsonParser.cs
@@ -6,11 +6,11 @@ namespace OpenCart_Testing.Tools
 {
     class JsonParser : AExternalReader
     {
-        public static T DeserializeFromFile<T>(string repository,string fileName)
+        public static T DeserializeFromFile<T>(string repository, string fileName)
         {
             try
             {
-                string json = File.ReadAllText(GetPath(repository,fileName));
+                string json = File.ReadAllText(GetPath(repository, fileName));
                 return JsonConvert.DeserializeObject<T>(json);
             }
             catch (Exception)
