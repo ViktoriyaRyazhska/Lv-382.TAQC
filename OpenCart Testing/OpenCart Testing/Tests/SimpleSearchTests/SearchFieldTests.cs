4661a81478c65522c9190d80c4396a72d170d2e4
SerhiiZakharchuk <serhiizakharchuk@gmail.com>
2019-04-03 12:17:23 +0300

    add jsons

--- OpenCart Testing/OpenCart Testing/Tests/SimpleSearchTests/SearchFieldTests.cs
+++ OpenCart Testing/OpenCart Testing/Tests/SimpleSearchTests/SearchFieldTests.cs
@@ -19,7 +19,8 @@ namespace OpenCart_Testing.Tests.SimpleSearchTests
 
         private static readonly object[] SearchData_Positive =
         {
-             new object[] { SimpleSearchRepository.NewSearchDataFromJson("SearchData_Case_DefaultView.json"), ProductRepository.GetMacListProducts()}
+             new object[] { SimpleSearchRepository.NewSearchDataFromJson("SearchData_Positive.json"),
+                 ProductRepository.GetMacListProducts()}
         };
 
         [Test, TestCaseSource("SearchData_Positive")]
@@ -27,22 +28,9 @@ namespace OpenCart_Testing.Tests.SimpleSearchTests
         {
             SearchCriteriaPage searchCriteriaPage = LoadApplication()
                 .SearchItems(searchText.SearchData);
-            Assert.AreEqual(Product.GetProductListNames(expectedList), searchCriteriaPage.GetProductComponentsContainer().GetProductComponentNames());
+            Assert.AreEqual(Product.GetProductListNames(expectedList),
+                searchCriteriaPage.GetProductComponentsContainer().GetProductComponentNames());
         }
-
-
-        //private static readonly object[] SearchData_Positive =
-        //{
-        //     new object[] {ProductRepository.GetMacBook(), ProductRepository.GetMacListProducts()}
-        //};
-
-        //[Test, TestCaseSource("SearchData_Positive")]
-        //public void SearchTest_Positive(Product product, IList<Product> expectedList)
-        //{
-        //    SearchCriteriaPage searchCriteriaPage = LoadApplication()
-        //        .SearchItems1(product);
-        //    Assert.AreEqual(Product.GetProductListNames(expectedList), searchCriteriaPage.GetProductComponentsContainer().GetProductComponentNames());
-        //}
 
 
         private static readonly object[] SearchData_Negative =
@@ -58,23 +46,6 @@ namespace OpenCart_Testing.Tests.SimpleSearchTests
         }
 
 
-        //private static readonly object[] SearchData_Negative =
-        //{
-        //    new TestCaseData(SimpleSearchRepository.NewSearchDataFromJson("SearchData_Negative.json"),
-        //        ProductRepository.Get().GetProductEmptyListMessage())
-        //};
-
-        //[Test, TestCaseSource("SearchData_Negative")]
-        //public void SearchTest_Negative(SimpleSearch searchText, string noexistTextMessage)
-        //{
-        //    SearchCriteriaPage searchCriteriaPage = LoadApplication()
-        //        .SearchItems(searchText.SearchData);
-        //    Assert.AreEqual(noexistTextMessage,
-        //        searchCriteriaPage.GetItemNotMatchesMessage());
-        //}
-
-
-        
         private static readonly object[] SearchData_InvalidLength =
             ListUtils.ToMultiArray(SimpleSearchRepository.NewDataListFromJson("SearchData_InvalidLength.json"));
 
@@ -89,39 +60,30 @@ namespace OpenCart_Testing.Tests.SimpleSearchTests
 
 
         private static readonly object[] SearchData_SpecialCharacters =
-        {
-            new TestCaseData(SimpleSearchRepository.NewSearchDataFromJson("SearchData_SpecialCharacters.json"),
-                ProductRepository.Get().GetProductEmptyListMessage())
-        };
+           ListUtils.ToMultiArray(SimpleSearchRepository.NewDataListFromJson("SearchData_SpecialCharacters.json"));
 
         [Test, TestCaseSource("SearchData_SpecialCharacters")]
-        public void SearchTest_SpecialCharacters(SimpleSearch searchText, string noneexistTextMessage)
+        public void SearchTest_SpecialCharacters(SimpleSearchView searchText)
         {
             SearchCriteriaPage searchCriteriaPage = LoadApplication()
-                .SearchItems(searchText.SearchData);
-            Assert.AreEqual(noneexistTextMessage,
+                .SearchItems(searchText.Name);
+            Assert.AreEqual(ProductRepository.Get().GetProductEmptyListMessage(),
                 searchCriteriaPage.GetItemNotMatchesMessage());
         }
 
 
         private static readonly object[] SearchData_Case_DefaultView =
-        {
-            new TestCaseData(SimpleSearchRepository.NewSearchDataFromJson("SearchData_Case_DefaultView.json"))
-            //SimpleSearchRepository.NewDataListFromJson("SearchData_Case_DefaultView.json")[0],
-            //SimpleSearchRepository.NewDataListFromJson("SearchData_Case_DefaultView.json")[1],
-            //SimpleSearchRepository.NewDataListFromJson("SearchData_Case_DefaultView.json")[2],
-            //SimpleSearchRepository.NewDataListFromJson("SearchData_Case_DefaultView.json")[3]
-        };
+            ListUtils.ToMultiArray(SimpleSearchRepository.NewDataListFromJson("SearchData_Case_DefaultViewActive.json"));
 
         [Test, TestCaseSource("SearchData_Case_DefaultView")]
-        public void SearchCaseInsensitive_Test(SimpleSearch searchText)
+        public void SearchCaseInsensitive_Test(SimpleSearchView searchText)
         {
             SearchCriteriaPage searchCriteriaPageLower = LoadApplication()
-                .SearchItems(searchText.SearchData.ToLower());
+                .SearchItems(searchText.Name.ToLower());
             IList<string> lowerSearchItems = searchCriteriaPageLower.GetProductComponentsContainer().GetProductComponentNames();
 
             SearchCriteriaPage searchCriteriaPageUpper = LoadApplication()
-                .SearchItems(searchText.SearchData.ToUpper());
+                .SearchItems(searchText.Name.ToUpper());
             IList<string> upperSearchItems = searchCriteriaPageUpper.GetProductComponentsContainer().GetProductComponentNames();
 
             Assert.AreEqual(lowerSearchItems, upperSearchItems);
