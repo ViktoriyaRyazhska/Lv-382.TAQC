56081768235dec8cbf4153a89258d757fe35824a
Oleksandra Yarmoliuk <sasha.yarmolyuk@gmail.com>
2019-04-03 12:15:40 +0300

    Class Diagram fixed

--- OpenCart Testing/OpenCart Testing/Tests/AddressBookTests/AddNewAddressTest.cs
+++ OpenCart Testing/OpenCart Testing/Tests/AddressBookTests/AddNewAddressTest.cs
@@ -1,12 +1,15 @@
 ﻿using NUnit.Framework;
+using OpenCart_Testing.Pages.AccountPages;
 using OpenCart_Testing.Pages.AddressBookPages;
 using OpenCart_Testing.TestData.AddressBookData;
+using OpenCart_Testing.TestData.LoginData;
 using OpenCart_Testing.Tools;
 
 namespace OpenCart_Testing.Tests.AddressBookTests
 {
     public class AddNewAddressTest : TestRunner
     {
+        private User myUser = LoginDataRespository.Get().GetUserLoginData("UserForAddressBookTests.json");
         private SuccessfullyUpdatedAddressPage updatedPage;
 
         private static readonly object[] ValidAddressData =
@@ -16,7 +19,7 @@ namespace OpenCart_Testing.Tests.AddressBookTests
         public void CheckAddingValidAddress(Address address)
         {
             AddressBookPage page = LoadApplication()
-               .ClickLoginUserButton().LoginUser(REGISTERED).GotoAddressBookPage();
+               .ClickLoginUserButton().LoginUser(myUser).GotoAddressBookPage();
 
             updatedPage = page.AddNewAddress().FillAddressAndContinue(address);
 
--- OpenCart Testing/OpenCart Testing/Tests/AddressBookTests/DeletingNotDefaultAddress.cs
+++ OpenCart Testing/OpenCart Testing/Tests/AddressBookTests/DeletingNotDefaultAddress.cs
@@ -1,15 +1,19 @@
 ﻿using NUnit.Framework;
+using OpenCart_Testing.Pages.AccountPages;
 using OpenCart_Testing.Pages.AddressBookPages;
+using OpenCart_Testing.TestData.LoginData;
 
 namespace OpenCart_Testing.Tests
 {
     class DeletingNotDefaultAddress: TestRunner
     {
+        private User myUser = LoginDataRespository.Get().GetUserLoginData("UserForAddressBookTests.json");
+
         [Test]
         public void CheckDeletingOfNotDefaultAddress()
         {
             AddressBookPage page = LoadApplication()
-                .ClickLoginUserButton().LoginUser(REGISTERED).GotoAddressBookPage();
+                .ClickLoginUserButton().LoginUser(myUser).GotoAddressBookPage();
             
             SuccessfullyUpdatedAddressPage updatedPage = page.DeleteSecondAddress();
 
