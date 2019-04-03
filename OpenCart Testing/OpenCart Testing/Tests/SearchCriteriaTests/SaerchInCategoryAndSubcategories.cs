f385a53f13e70dd53dc4b3ffe73f622b890f8e5d
Roman Pihel <romanpihel1992@gmail.com@>
2019-04-03 11:15:11 +0300

    add product repository Json

---
+++ OpenCart Testing/OpenCart Testing/Tests/SearchCriteriaTests/SaerchInCategoryAndSubcategories.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using OpenCart_Testing.Pages;
+using OpenCart_Testing.TestData;
+using System.Collections.Generic;
+
+namespace OpenCart_Testing.Tests.SearchCriteriaTests
+{
+    [TestFixture]
+    public class SearchInCategoryAndSubcategoriesTest : TestRunner
+    {
+        private static readonly object[] ProductCategoryData =
+        {
+            new TestCaseData(SearchCriteriasRepository.Get().NewSearchCriteriaFromJson("SearchInCategory.json"), ProductRepository.NewProductArrayFromJson("ProductsSearchInCategory.json") )
+        };
+
+        [Test, TestCaseSource(nameof(ProductCategoryData))]
+        public void CheckSearchInCategory(ISearchCriteria searchCriteria, IList<Product> expectedList)
+        {            
+            SearchCriteriaPage searchCriteriaPage = LoadApplication()
+                .GoToSearchCriteriaPage().SearchCriteriaItems(searchCriteria);          
+            Assert.AreEqual(Product.GetProductListNames(expectedList), searchCriteriaPage.GetProductComponentsContainer().GetProductComponentNames());     
+        }
+
+
+        private static readonly object[] ProductSubcategoryData =
+        {
+            new TestCaseData(SearchCriteriasRepository.Get().NewSearchCriteriaFromJson("SearchInCategoryAndSubcategories.json"), ProductRepository.NewProductArrayFromJson("ProductsSearchInCategoryAndSubcategory.json") )
+        };
+
+        [Test, TestCaseSource(nameof(ProductSubcategoryData))]
+        public void CheckSearchInCategoryAndSubcategories(ISearchCriteria searchCriteria, IList<Product> expectedList)
+        {           
+            SearchCriteriaPage searchCriteriaPage = LoadApplication()
+                .GoToSearchCriteriaPage().SearchCriteriaItems(searchCriteria);
+            Assert.AreEqual(Product.GetProductListNames(expectedList), searchCriteriaPage.GetProductComponentsContainer().GetProductComponentNames());        
+        }
+    }
+}
--- OpenCart Testing/OpenCart Testing/Tests/SearchCriteriaTests/SearchInDescription.cs
+++ OpenCart Testing/OpenCart Testing/Tests/SearchCriteriaTests/SearchInDescription.cs
@@ -1,53 +1,57 @@
 ﻿using NUnit.Framework;
 using OpenCart_Testing.Pages;
 using OpenCart_Testing.TestData;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OpenCart_Testing.Tests.SearchCriteriaTests
 {
     [TestFixture]
     public class SearchInDescriptionTest : TestRunner
     {
-        private static readonly object[] ProductNoInDescriptionData =
-       {
-            new TestCaseData(SearchCriteriasRepository.Get().NewSearchCriteriaFromJson("SearchNoInDescription.json"), ProductRepository.Get().GetProductEmptyListMessage() )
+        private static readonly object[] SearchNotInDescriptionData =
+        {
+            new TestCaseData(SearchCriteriasRepository.Get().NewSearchCriteriaFromJson("SearchNotInDescription.json"), ProductRepository.Get().GetProductEmptyListMessage() )
         };
 
-        [Test, TestCaseSource(nameof(ProductNoInDescriptionData))]
-        public void CheckSearchWithoutInDescription(ISearchCriteria searchCriteria, string expectedMessage)
+        [Test, TestCaseSource(nameof(SearchNotInDescriptionData))]
+        public void CheckSearchNotInDescription(ISearchCriteria searchCriteria, string expectedMessage)
         {
-
             SearchCriteriaPage searchCriteriaPage = LoadApplication()
                 .GoToSearchCriteriaPage().SearchCriteriaItems(searchCriteria);
 
             Assert.AreEqual(ProductRepository.Get().GetProductEmptyListMessage(), searchCriteriaPage.GetProductComponentsContainer().GetEmptyListMessange());
-            
-            HomePage homePage = searchCriteriaPage.GotoHomePage();
-
-            Assert.IsTrue(homePage.GetSlideshow0FirstImageAttributeSrcText().Contains(HomePage.IPHONE6));
         }
 
-        private static readonly object[] ProductDescriptioData =
-       {
-            new TestCaseData(SearchCriteriasRepository.Get().NewSearchCriteriaFromJson("SearchInDescription.json"), ProductRepository.GetSearchInDescriptionProducts() )
+        private static readonly object[] SearchInDescriptionData =
+        {
+            new TestCaseData(SearchCriteriasRepository.Get().NewSearchCriteriaFromJson("SearchInDescription.json"), ProductRepository.NewProductArrayFromJson("ProductsSearchInDescription.json") )
         };
 
-        [Test, TestCaseSource(nameof(ProductDescriptioData))]
+        [Test, TestCaseSource(nameof(SearchInDescriptionData))]
         public void CheckSearchInDescription(ISearchCriteria searchCriteria, IList<Product> expectedList)
         {
-
             SearchCriteriaPage searchCriteriaPage = LoadApplication()
                 .GoToSearchCriteriaPage().SearchCriteriaItems(searchCriteria);
 
             Assert.AreEqual(Product.GetProductListNames(expectedList), searchCriteriaPage.GetProductComponentsContainer().GetProductComponentNames());
-
-         //   HomePage homePage = searchCriteriaPage.GotoHomePage();
-
-         //   Assert.IsTrue(homePage.GetSlideshow0FirstImageAttributeSrcText().Contains(HomePage.IPHONE6));
         }
+
+        //private static readonly object[] ProductDescriptionData =
+        //{
+        //    new TestCaseData(SearchCriteriasRepository.NewSearchCriteriaArrayFromJson("SearchByDescription.json"), ProductRepository.NewProductArrayFromJson("ProductsSearchInDescription.json") )
+        //};
+
+        //[Test, TestCaseSource(nameof(ProductDescriptionData))]
+        //public void CheckSearchDescription(IList<SearchCriteria> searchCriteria, IList<Product> expectedList)
+        //{
+
+        //    SearchCriteriaPage searchCriteriaPage = LoadApplication()
+        //        .GoToSearchCriteriaPage().SearchCriteriaItems(searchCriteria[0]);
+        //    Assert.AreEqual(ProductRepository.Get().GetProductEmptyListMessage(), searchCriteriaPage.GetProductComponentsContainer().GetEmptyListMessange());
+
+        //    searchCriteriaPage.GotoHomePage().GoToSearchCriteriaPage().SearchCriteriaItems(searchCriteria[1]);           
+        //    Assert.AreEqual(Product.GetProductListNames(expectedList), searchCriteriaPage.GetProductComponentsContainer().GetProductComponentNames());
+            
+        //}
     }
 }
