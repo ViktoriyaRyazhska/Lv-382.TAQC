bac5d1c43b3fb541b80f35a904cf00e695d49bc5
imokhnii <officialmokhnii@gmail.com>
2019-04-03 12:39:50 +0300

    Ukrnet PageObject added ForgottenPassword added

---
+++ OpenCart Testing/OpenCart Testing/Pages/UIMapping/MForgottenPasswordPage.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+
+namespace OpenCart_Testing.Pages.UIMapping
+{
+    class MForgottenPasswordPage
+    {
+        public static By locatorEmailInputField => By.XPath("//input[@id = 'input-email']");
+        public static By locatorContinueButton => By.XPath("//input[@class = 'btn btn-primary']");
+        public static By locatorBackButton => By.XPath("//div[@class = 'pull-left']/a");
+    }
+}
---
+++ OpenCart Testing/OpenCart Testing/Pages/UkrnetPage/AUkrnetLeftMenuPart.cs
@@ -0,0 +1,30 @@
+﻿using OpenCart_Testing.Pages.UIMapping;
+using OpenQA.Selenium;
+
+namespace OpenCart_Testing.Pages.UkrnetPage
+{
+    public abstract class AUkrnetLeftMenuPart
+    {
+        protected IWebDriver driver;
+
+        public AUkrnetLeftMenuPart(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        protected IWebElement Inbox
+        { get { return driver.FindElement(MUkrnetElemntsPart.locatorInbox); } }
+        protected IWebElement Draft
+        { get { return driver.FindElement(MUkrnetElemntsPart.locatorDrafts); } }
+        protected IWebElement Deleted
+        { get { return driver.FindElement(MUkrnetElemntsPart.locatorDeleted); } }
+        protected IWebElement Sent
+        { get { return driver.FindElement(MUkrnetElemntsPart.locatorSent); } }
+        protected IWebElement Spam
+        { get { return driver.FindElement(MUkrnetElemntsPart.locatorSpam); } }
+        protected IWebElement Unread
+        { get { return driver.FindElement(MUkrnetElemntsPart.locatorUnread); } }
+        protected IWebElement Marked
+        { get { return driver.FindElement(MUkrnetElemntsPart.locatorMarked); } }
+    }
+}
