821415369534b8e1550e683238124c83b1dd95cd
bebebe <hahaha@gmail.com>
2019-04-16 18:11:17 +0300

    Add Allure

--- OpenCartTestProject/SimpleTest.cs
+++ OpenCartTestProject/SimpleTest.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.IO;
 using System.Threading;
+using Allure.Commons;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NLog;
+using NUnit.Allure.Attributes;
+using NUnit.Allure.Core;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -9,10 +14,49 @@ using OpenQA.Selenium.Interactions;
 
 namespace OpenCartTestProject
 {
+    [AllureNUnit]
+    [AllureDisplayIgnored]
     //[TestClass]
     [TestFixture]
     public class SimpleTest
     {
+        private const string FOLDER_BIN = "bin";
+        private const string ALLURE_CONFIG = "allureConfig.json";
+        //
+        public static Logger log = LogManager.GetLogger("rolling0"); // nlog
+
+        [OneTimeSetUp]
+        public void BeforeAllMethods()
+        {
+            log.Info("BeforeAllMethods() Start");
+            //PrepareAllureConfig();
+            log.Info("BeforeAllMethods() Done");
+        }
+
+        private void PrepareAllureConfig()
+        {
+            log.Debug("PrepareAllureConfig() Start");
+            string runtimePath = AppDomain.CurrentDomain.BaseDirectory;
+            string sourcePath = runtimePath.Remove(runtimePath.IndexOf(FOLDER_BIN)) + ALLURE_CONFIG;
+            log.Trace("runtimePath = " + runtimePath
+                + "%n\t sourcePath = " + sourcePath
+                + "%n\t runtimePath + ALLURE_CONFIG" + runtimePath + ALLURE_CONFIG);
+            if (File.Exists(sourcePath))
+            {
+                if (File.Exists(runtimePath + ALLURE_CONFIG))
+                {
+                    File.Delete(runtimePath + ALLURE_CONFIG);
+                }
+                File.Copy(sourcePath, runtimePath + ALLURE_CONFIG);
+                log.Trace("PrepareAllureConfig() Copy Done");
+            }
+            else
+            {
+                log.Warn("File " + sourcePath + " NOT FOUND.");
+            }
+            log.Debug("PrepareAllureConfig() Done");
+        }
+
         //[TestMethod]
         //[Test]
         public void CheckLogin()
@@ -120,6 +164,53 @@ namespace OpenCartTestProject
             Thread.Sleep(4000); // For Presentation ONLY
             //
             driver.Quit();
+        }
+
+        [Test]
+        [AllureTag("Regression_Tag")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureIssue("HI-61")]
+        [AllureTms("TMS-12")]
+        [AllureOwner("User_Owner")]
+        [AllureParentSuite("With_parameters_ParentSuite")]
+        [AllureSuite("Passed_Suite")]
+        [AllureSubSuite("NoAssert_SubSuite")]
+        [AllureEpic("Retry_Epic")]
+        [AllureFeature("RetrySmall_Feature")]
+        [AllureLink("softserve_ITA_Link", "https://softserve.academy/")]
+        public void SoftserveAcademy()
+        {
+            log.Info("SoftserveAcademy() Start");
+            log.Info("ThreadID = " + Thread.CurrentThread.ManagedThreadId);
+            //
+            IWebDriver driver;
+            driver = new ChromeDriver();
+            //driver = new FirefoxDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10); // by default = 0
+            //
+            driver.Navigate().GoToUrl("https://softserve.academy/");
+            Thread.Sleep(1000); // DO NOT USE
+            //
+            driver.FindElement(By.LinkText("Log in")).Click();
+            Thread.Sleep(1000); // DO NOT USE
+            //
+            driver.FindElement(By.Id("username")).Click();
+            driver.FindElement(By.Id("username")).Clear();
+            driver.FindElement(By.Id("username")).SendKeys("Hello2");
+            Thread.Sleep(2000); // DO NOT USE
+            //
+            driver.FindElement(By.Id("username")).Click();
+            driver.FindElement(By.Id("username")).Clear();
+            driver.FindElement(By.Id("username")).SendKeys("Hello");
+            Thread.Sleep(2000); // DO NOT USE
+            //
+            driver.FindElement(By.Id("password")).Click();
+            driver.FindElement(By.Id("password")).Clear();
+            driver.FindElement(By.Id("password")).SendKeys("qwerty");
+            Thread.Sleep(2000); // DO NOT USE
+            //
+            driver.Quit();
+            log.Info("SoftserveAcademy() Done");
         }
     }
 }
