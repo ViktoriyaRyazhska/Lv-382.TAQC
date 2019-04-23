00ecfc205486f59134f6e976e69cb5951c32b7dd
imokhnii <officialmokhnii@gmail.com>
2019-04-23 23:22:46 +0300

    Json Part added Logger moved to tools ConvertToResource moved to tools

--- RestTestProject/Data/UserData/UserRepository.cs
+++ RestTestProject/Data/UserData/UserRepository.cs
@@ -1,9 +1,12 @@
-﻿namespace RestTestProject.Data
+﻿using RestTestProject.Tools;
+
+namespace RestTestProject.Data
 {
     public sealed class UserRepository
     {
-        public static string USER_NOT_FOUND_ERROR = "ERROR, user not found";
-        public static string USER_LOCKED_ERROR = "ERROR, user locked";
+        public const string USER_NOT_FOUND_ERROR = "ERROR, user not found";
+        public const string USER_LOCKED_ERROR = "ERROR, user locked";
+        private const string directory = "UsersCredentials";
         private volatile static UserRepository instance;
         private static object lockingObject = new object();
 
@@ -23,7 +26,7 @@
             }
             return instance;
         }
-
+        /*
         public IUser ExistingAdmin()
         {
             return User.Get().SetName("admin").SetPassword("qwerty").SetRigths(true.ToString()).Build();
@@ -42,6 +45,26 @@
         public IUser NonExistentAdmin()
         {
             return User.Get().SetName("SomeAdmin").SetPassword("qwerty").SetRigths(true.ToString()).Build();
+        }*/
+
+        public IUser ExistingAdmin()
+        {
+            return JsonParser.DeserializeFromFile<User>(directory, "ExistingAdminData.json");
+        }
+
+        public IUser ExistingUser()
+        {
+            return JsonParser.DeserializeFromFile<User>(directory, "ExistingUserData.json");
+        }
+
+        public IUser NonExistentAdmin()
+        {
+            return JsonParser.DeserializeFromFile<User>(directory, "NonExistentAdminData.json");
+        }
+
+        public IUser NonExistentUser()
+        {
+            return JsonParser.DeserializeFromFile<User>(directory, "NonExistentUserData.json");
         }
 
     }
---
+++ RestTestProject/Tools/AExternalReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RestTestProject.Tools
+{
+    public abstract class AExternalReader
+    {
+        protected const string CONVERT_OBJECT_ERROR = "ConvertToObject Error. {0}\n{1}";
+        public const string PATH_SEPARATOR = "\\";
+        protected const string FOLDER_DATA = "Data\\DataSource";
+        protected const string FOLDER_BIN = "bin";
+
+        public static string FilePath { get; protected set; }
+
+        protected static string GetPath(string fileDirectory, string filename)
+        {
+            FilePath = AppDomain.CurrentDomain.BaseDirectory;
+            return FilePath.Remove(FilePath.IndexOf(FOLDER_BIN)) + FOLDER_DATA + PATH_SEPARATOR + fileDirectory + PATH_SEPARATOR + filename;
+        }
+    }
+}
